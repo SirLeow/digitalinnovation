d93102f8884ab9de228ab309bb8c1cd754596c17
Thiago Barros <t.b.lima18@gmail.com>
2022-02-10 11:23:17 -0300

    Atulização para tornar privado o set da variavel cpf

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,7 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Sother"
     var cpf:String = "123.234.112-23"
+    private set //torna incapaz a alteração do falor do cpf fora da classe Pessoa
 
     inner class Endereco(){
         var rua:String = "Travessa Angustura"
